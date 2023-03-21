1a79ac1105017183a906511359cbc7eab73c47c6
mmengdi <898581578@qq.com>
2023-03-22 00:30:00 +0800

    Site updated: 2023-03-22 00:29:56

--- js/title.js
+++ js/title.js
@@ -4,11 +4,11 @@ var titleTime;
 document.addEventListener('visibilitychange', function () {
     if (document.hidden) {
         //离开当前页面时标签显示内容
-        document.title = '👀跑哪里去了！！！';
+        document.title = '怎么走开啦！😭😭';
         clearTimeout(titleTime);
     } else {
         //返回当前页面时标签显示内容
-        document.title = '🐖抓到你啦！！！';
+        document.title = '你来啦！😊😊';
         //两秒后变回正常标题
         titleTime = setTimeout(function () {
             document.title = OriginTitile;
